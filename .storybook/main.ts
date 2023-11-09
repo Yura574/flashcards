a7535518ffa6d6f853f1971fed385a44a502b028
Yura <yura5742248@gmail.com>
2023-11-09 19:20:33 +0300

    feat: add styles

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type {StorybookConfig} from '@storybook/react-vite';
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
 
 const config: StorybookConfig = {
   "stories": [
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,12 +1,15 @@
-import type { Preview } from '@storybook/react'
+import type {Preview} from '@storybook/react'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import '../src/styles/_index.scss'
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
+    actions: {argTypesRegex: '^on[A-Z].*'},
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
